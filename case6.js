fa5cfc75f855c4439d8efbc66c016bf65aee297c
nirvanadam <nirvan.pramudhyta@gmail.com>
2023-03-17 23:29:59 +0700

    fix condition state

--- case6.js
+++ case6.js
@@ -1,20 +1,18 @@
 function getAngkaTerbesarKedua(dataNumbers) {
-  let maxNumber = -Infinity;
-  let secondMaxNumber = -Infinity;
+  if (dataNumbers === undefined) {
+    return "ERROR: There is no parameter!";
+  } else if (typeof dataNumbers !== "object") {
+    return "ERROR: Invalid type data! It must be an array!";
+  } else {
+    sortedNumber = dataNumbers.sort();
+    secondBiggestNumber = sortedNumber[sortedNumber.length - 2];
+  }
 
-  dataNumbers.forEach((number) => {
-    if (number > maxNumber) {
-      secondMaxNumber = maxNumber;
-      maxNumber = number;
-    } else if (number > secondMaxNumber && number !== maxNumber) {
-      secondMaxNumber = number;
-    }
-  });
-  return secondMaxNumber;
+  return secondBiggestNumber;
 }
 
 const dataAngka = [9, 4, 7, 7, 4, 3, 2, 2, 8];
-console.log(getAngkaTerbesarKedua(dataAngka));
 
+console.log(getAngkaTerbesarKedua(dataAngka));
 console.log(getAngkaTerbesarKedua(0));
 console.log(getAngkaTerbesarKedua());
